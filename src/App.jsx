165d8c318b28c5772d80a7c3b8542e57a0484afb
GG-Pharaoh <oxdiablo20@gmail.com>
2024-08-19 01:54:55 -0700

    Mounted first component + UI Basics

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ToDo from './components/ToDo'
+
+
+const App = () => {
+  return (
+    <div className='bg-stone-900 grid py-4 min-h-screen'>
+
+    <ToDo /> 
+  
+
+  </div>
+  )
+}
+
+export default App
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import todo_icon from '../assets/todo_icon.png'
+
+const 
+
+ToDo 
+= () => {
+  return (
+    <div className=' bg-white place-self-center w-11/12 
+    max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
+
+        {/* Title */}
+
+        <div className='flex items-center mt-7 gap-2'>
+          <img src={todo_icon} className='w-8'/>
+            <h1  className='text-3xl font-semibold'> To-Do List </h1>
+        </div>
+
+          {/* Input Box */}
+
+          <div className='flex items-center my-7 bg-gray-200' >
+          <input className= 'bg-transparent border-0 outline-none
+          flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' 
+          type="text" placeholder='Add Your task'/>
+            <button className='border-none rounded-full
+             bg-orange-600 w-32 h-14 text-white text-lg
+             font-medium cursor-pointer'> ADD + </button>
+          </div>
+
+
+            {/* Todo Lists */}
+
+            <div > 
+
+
+            </div>
+
+
+
+         
+
+    </div>
+  )
+}
+
+export default 
+
+ToDo 
