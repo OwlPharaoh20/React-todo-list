32ad1b8e259bc71a16ad1b5aa8042756039bacdd
GG-Pharaoh <oxdiablo20@gmail.com>
2024-08-19 05:13:03 -0700

    functionality phase, yet to fix toggle button in ToDoItems component

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
 
-const 
-
-ToDo 
+const ToDo 
 = () => {
+
+  const [todoList, setTodoList] = useState([]);
+
+  const inputRef = useRef();
+
+const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+
+    }
+    const newTodo = {
+        id : Date.now(),
+        text: inputText,
+        isComplete: false,
+    }
+
+  setTodoList((prev) => [...prev, newTodo]);
+  inputRef.current.value = "";
+
+} 
+
+const deleteTodo = (id) => {
+  setTodoList ((prvTodos) => {
+   return prvTodos.filter((todo) => todo.id !== id)
+  })
+}
+
+
+const toggle = () => {
+      setTodoList((prevTodos) => {
+        return prevTodos.map( (todo) => {
+      if(todo.id === id) {
+       return {...todo, isComplete: !todo.isComplete}
+      }
+      return todo;
+        })
+
+
+      })
+
+}
+
+
+useEffect(() => {
+  console.log(todoList);
+  
+}, [todoList])
+
   return (
     <div className=' bg-white place-self-center w-11/12 
     max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
@@ -20,10 +68,10 @@ ToDo
           {/* Input Box */}
 
           <div className='flex items-center my-7 bg-gray-200' >
-          <input className= 'bg-transparent border-0 outline-none
-          flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' 
-          type="text" placeholder='Add Your task'/>
-            <button className='border-none rounded-full
+          <input  ref={inputRef} className='bg-transparent border-0 outline-none
+          flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text"
+           placeholder='Add Your task'/>
+            <button onClick={add} className='border-none rounded-full
              bg-orange-600 w-32 h-14 text-white text-lg
              font-medium cursor-pointer'> ADD + </button>
           </div>
@@ -32,9 +80,12 @@ ToDo
             {/* Todo Lists */}
 
             <div > 
-              <TodoItems text=" Learn Coding" />
-              <TodoItems text=" Learn Coding From GreatStack"/>
-              
+
+              {todoList.map((item, index) => {
+              return <TodoItems key={index} text={item.text}
+              id={item.id} isComplete={item.isComplete} deleteTodo={deleteTodo}
+              toggle={toggle}/>
+              })}
 
 
             </div>
